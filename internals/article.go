1b06b329d1825089c39134e0dcea16a7bc9863ad
maxchichar <maxwellchibuezecharles@gmail.com>
2026-04-19 23:38:10 +0100

    Article edge case handled

--- internals/article.go
+++ internals/article.go
@@ -9,6 +9,7 @@ import(
 func ApplyAnRule(tokens []string) []string {
 	token := strings.Join(tokens, " ")
 	token = regexp.MustCompile(`(?i)\b(a)\s+([aeiouhAEIOUH])`).ReplaceAllString(token, "an $2")
+	token = regexp.MustCompile(`(?i)\ban\s+([bcdfgjklmnpqrstvwxyzh])`).ReplaceAllString(token, "a $1")
 	token = regexp.MustCompile(`(?i)\ban (university|useful|useless|unicorn|uniform|union|unique|user|European)\b`).ReplaceAllString(token, "a $1")
 	return strings.Fields(token)
 }
